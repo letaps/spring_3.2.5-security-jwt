103f56749b733540b6e32c48fdf650bd5dd89aa7
LeTapiwa Chibz <tapschibz@gmail.com>
2024-05-11 09:23:10 +0200

    :alembic: Code Smells

--- src/main/java/com/chibindiTechnologies/smartStationBE/repository/UserInfoRepository.java
+++ src/main/java/com/chibindiTechnologies/smartStationBE/repository/UserInfoRepository.java
@@ -2,11 +2,6 @@ package com.chibindiTechnologies.smartStationBE.repository;
 
 import com.chibindiTechnologies.smartStationBE.enitity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
-
-import java.util.Optional;
-
 public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
     UserInfo findFirstByUsernameOrderByIdDesc(String username);
 
